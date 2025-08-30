ea8139933cbccc96a9ae2258096b371b2dbfdb73
ABINESH-K-7 <abineshkumar.pp@gmail.com>
2025-08-30 19:53:24 +0530

    projected completed

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/assets';
 import Message from './Message';
+import Credits from '../pages/Credits';
+import toast from 'react-hot-toast';
 
 const ChatBox = () => {
   const containerRef = useRef(null)
 
- const {selectedChat,theme} = useAppContext();
+ const {selectedChat,theme, user, axios, token, setUser} = useAppContext();
 
  const [messages,setMessages] =useState([]);
  const [loading,setLoading] =useState(false);
@@ -16,7 +18,38 @@ const [mode,setMode] = useState('text');
 const [isPublished,setIsPublished] = useState(false);
 
 const onSubmit = async (e)=>{
-   e.preventDefault()
+  try {
+     e.preventDefault();
+     if(!user) return toast('Login to send message')
+      setLoading(true)
+      const promptCopy = prompt
+      setPrompt('');
+      setMessages((prev)=>[...prev, {role: 'user', content: prompt, timestamp:Date.now(), isImage: false }])  
+      const {data} = await axios.post(`/api/message/${mode}`, {chatId:selectedChat._id, prompt, isPublished}, {headers: { Authorization:token}}) 
+  
+    if(data.success)
+    {
+      setMessages(prev=> [...prev, data.reply])
+      //  decrease Credits
+      if(mode==='image')
+      {
+        setUser(prev => ({...prev, credits:prev.credits-2}))
+      }
+      else{
+        setUser(prev => ({...prev, credits:prev.credits-1}))
+      }
+    }
+   else{
+    toast.error(data.message);
+    setPrompt(promptCopy)
+   }
+    } catch (error) {
+    toast.error(error.message);
+   }
+   finally{
+    setPrompt('');
+    setLoading(false)
+   }
 }
 
 useEffect(()=>{
@@ -33,7 +66,7 @@ useEffect(()=>{
     {
       containerRef.current.scrollTo({
         top: containerRef.current.scrollHeight, 
-        behaviour: "smooth"
+        behavior: "smooth"
       })
     }
 },[messages])
@@ -41,37 +74,37 @@ useEffect(()=>{
   return (
     <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
       {/* Chat Messages */}
-      <div ref={containerRef} className='flex-1 mb-5 overflow-y-scroll'>
+      <div ref={containerRef} className=' flex-1 mb-5 overflow-y-scroll flex flex-col w-full '>
         {messages.length === 0 && (
-         <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
-          <img src={theme === 'dark' ? assets.logo_full:assets.logo_full_dark } alt=""  className='w-full max-w-56 sm:max-w-68' />
-          <p className='mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-white'>Ask Me Anything!!!</p>
-         </div>
+            <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
+              <img src={theme === 'dark' ? assets.logo_full:assets.logo_full_dark } alt=""  className='w-full max-w-56 sm:max-w-68 ' />
+              <p className='mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-white'>Ask Me Anything!!!</p>
+            </div>
 
         )}
    {messages.map((message, index)=><Message key={index} message={message}/>)}
 
  {/* Three Dots Loading Animation */}
  
-  {
-    loading && <div className='loader flex items-center gap-1.5'>
-      <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
-      <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
-      <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
-    </div>
-  }
+            {
+              loading && <div className='loader flex items-center gap-1.5'>
+                <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
+                <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
+                <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
+              </div>
+            }
       </div>
 
 {mode === 'image' && (
   <label className='inline-flex items-center gap-2 mb-3 text-sm mx-auto'>
-  <p className='text-xs'>Publish Generated Image to Comunnity </p>
+  <p className='text-xs'>Publish Generated Image to Community </p>
   <input type="checkbox" className='cursor-pointer'  checked={isPublished} onChange={(e)=>setIsPublished(e.target.checked)}  />
   </label>
 )}
 
 {/* Prompt Input Box */}
 
-  <form onSubmit={onSubmit} className='bg-primary/20 dark:bg-[#583C79]/30 border border-primary dark:border-[#80609F]/30 rounded-full w-full max-w-2xl p-3 pl-4 mx-auto flex gap-4, items-center' >
+  <form onSubmit={onSubmit} className='bg-primary/20 dark:bg-[#583C79]/30 border border-primary dark:border-[#80609F]/30 rounded-full w-full max-w-2xl p-3 pl-4 mx-auto flex gap-4 items-center' >
 
     <select onChange={(e)=>setMode(e.target.value)} value={mode} className='text-sm pl-3 pr-2 outline-none' id="">
       <option className='dark:bg-purple-900' value="text">Text</option>
